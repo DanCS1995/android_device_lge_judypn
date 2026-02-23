2d0dfcf07ad740a10d5dee79bf981d723c54fcac
DanCS1995 <danielcsouza1995@gmail.com>
2026-02-23 00:32:02 -0300

    judypn: drop LineageOS-specific overlays and integrations
    Clean up LineageOS-specific integrations from judypn.
    
    - Remove unused overlays and framework hooks
    - Drop Lineage-only configuration remnants
    - Preserve vendor HAL and hardware interfaces
    
    Alignment with PixelOS/AOSP base.

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ $(call inherit-product, device/lge/sdm845-common/sdm845.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+#   $(LOCAL_PATH)/overlay-lineage
 
 # Audio
 PRODUCT_COPY_FILES += \
